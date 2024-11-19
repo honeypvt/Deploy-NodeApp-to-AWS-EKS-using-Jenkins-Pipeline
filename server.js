f55c57f06fe79f0108b6df01839d13987265c77d
honeypvt <90208900+honeypvt@users.noreply.github.com>
2024-11-19 16:24:50 +0530

    Update server.js 1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('This site is deployed by kusha kumar using jenkins docker and AWS EKS services');
 });
 
 app.listen(PORT, HOST);
